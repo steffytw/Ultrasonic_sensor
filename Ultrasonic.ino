b542da4748052f97ae45edb0aedea61780e38124
steffytw <33021781+steffytw@users.noreply.github.com>
2019-02-17 09:27:27 +0530

    Add files via upload

---
+++ Ultrasonic.ino
@@ -0,0 +1,50 @@
+int trigpin = 9;
+int echopin = 10;
+int led1 = 7;
+int led2 = 8;
+int led3 = 4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(trigpin,OUTPUT);
+  pinMode(echopin,INPUT);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  long duration,distance;
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigpin,LOW);
+  duration=pulseIn(echopin,HIGH);
+  distance =(duration/2)/29.1;
+  Serial.print(distance); 
+  Serial.println("CM");
+  delay(10);
+  if((distance<15))    //red
+  {
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+    }
+    else if(distance>100)    //ylw
+    {
+    
+    digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,HIGH);
+      }
+      else             //grn
+      {
+        digitalWrite(led1,LOW);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,LOW);
+        }
+  // put your main code here, to run repeatedly:
+
+}
